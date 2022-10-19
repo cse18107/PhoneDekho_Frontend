31c3281917df011966fed5389211630ba9eec8ef
cse18107 <cse18107@cemk.ac.in>
2022-10-19 19:42:14 +0530

    working on dashboard

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Dashboard.css';
+
+const Dashboard = () => {
+  return (
+    <div className='dashboard-body'>
+      <div className='dashboard-headers'>
+        <div>logo</div>
+      </div>
+      <div className='dashboard-container'>
+        <div className='dashboard-left__sidebar'>
+          <div className='dashboard-left__sidebar-button'>Your profile</div>
+          <div className='dashboard-left__sidebar-button'>Dashboard</div>
+          <div className='dashboard-left__sidebar-button'>Users details</div>
+          <div className='dashboard-left__sidebar-button'>Items Details</div>
+          <div className='dashboard-left__sidebar-button'>Add Items</div>
+          <div className='dashboard-left__sidebar-button'>Orders Details</div>
+        </div>
+        <div className='dashboard-right__content'>
+          <div className='falt'>asdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsffasdfsaddfgasdgasdfasdfasdfasdfasdfdfasdfasdfaweagasgasdgasdasdfasdfasdfasdfsadfsff</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard;
