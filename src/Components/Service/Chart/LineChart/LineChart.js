7efddcf39fa2788cfeeddd5a7049f7a71cc5eaf0
cse18107 <cse18107@cemk.ac.in>
2022-10-30 22:00:46 +0530

    working on code optimization in dashboard

---
+++ src/Components/Service/Chart/LineChart/LineChart.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import '../index.css';
+import { CChart } from "@coreui/react-chartjs";
+
+const LineChart = () => {
+  return (
+    <div className="chart_2">
+          <div className="chart_header">
+            <div className="chart_heading">Chart heading</div>
+            <div className="chart_query">
+              <select>
+                <option>Select year</option>
+                <option>2001</option>
+                <option>2002</option>
+                <option>2003</option>
+                <option>2004</option>
+              </select>
+            </div>
+          </div>
+          <div className="chart-content">
+            <CChart
+              type="line"
+              data={{
+                labels: [
+                  "January",
+                  "February",
+                  "March",
+                  "April",
+                  "May",
+                  "June",
+                  "July",
+                ],
+                datasets: [
+                  {
+                    label: "My First dataset",
+                    backgroundColor: "rgba(220, 220, 220, 0.2)",
+                    borderColor: "rgba(220, 220, 220, 1)",
+                    pointBackgroundColor: "rgba(220, 220, 220, 1)",
+                    pointBorderColor: "#fff",
+                    data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
+                  },
+                  {
+                    label: "My Second dataset",
+                    backgroundColor: "rgba(151, 187, 205, 0.2)",
+                    borderColor: "rgba(151, 187, 205, 1)",
+                    pointBackgroundColor: "rgba(151, 187, 205, 1)",
+                    pointBorderColor: "#fff",
+                    data: [50, 12, 28, 29, 7, 25, 12, 70, 60],
+                  },
+                ],
+              }}
+            />
+          </div>
+        </div>
+  )
+}
+
+export default LineChart
--- src/Pages/Dashboard/GraphPage/GraphPage.js
+++ src/Pages/Dashboard/GraphPage/GraphPage.js
@@ -1,222 +1,82 @@
-import React from 'react'
+import React from "react";
 import { CChart } from "@coreui/react-chartjs";
 import { PieChart } from "react-minimal-pie-chart";
-import Wrapper from '../../../Components/Wrapper/Wrapper';
-import './GraphPage.css';
-import user from '../../../assets/icons/user_logo.png'
-import products from '../../../assets/icons/products.png'
-import orders from '../../../assets/icons/order.png'
-import delevered from '../../../assets/icons/delevered.png'
+import Wrapper from "../../../Components/Wrapper/Wrapper";
+import "./GraphPage.css";
+import user from "../../../assets/icons/user_logo.png";
+import products from "../../../assets/icons/products.png";
+import orders from "../../../assets/icons/order.png";
+import delevered from "../../../assets/icons/delevered.png";
+import { setBg } from "../../../utils/getRandomColor";
+import BarChart from "../../../Components/Service/Chart/BarChart/BarChart";
+import PieDataChart from "../../../Components/Service/Chart/PieChart/PieDataChart";
+import LineChart from "../../../Components/Service/Chart/LineChart/LineChart";
+
+const pieChartData = [
+  { title: "Apple", value: 10, color: `${setBg()}`, label: "one" },
+  { title: "Samsung", value: 15, color: `${setBg()}`, label: "two" },
+  { title: "Google", value: 20, color: `${setBg()}`, label: "three" },
+  { title: "LG", value: 10, color: `${setBg()}`, label: "three" },
+  { title: "Motorola", value: 31, color: `${setBg()}`, label: "three" },
+  { title: "OnePlus", value: 36, color: `${setBg()}`, label: "three" },
+  { title: "Apple", value: 10, color: `${setBg()}`, label: "one" },
+  { title: "Samsung", value: 15, color: `${setBg()}`, label: "two" },
+  { title: "Google", value: 20, color: `${setBg()}`, label: "three" },
+  { title: "LG", value: 10, color: `${setBg()}`, label: "three" },
+  { title: "Motorola", value: 31, color: `${setBg()}`, label: "three" },
+  { title: "OnePlus", value: 36, color: `${setBg()}`, label: "three" },
+  { title: "Apple", value: 10, color: `${setBg()}`, label: "one" },
+  { title: "Samsung", value: 15, color: `${setBg()}`, label: "two" },
+];
+const companyName = ["Apple", "Samsung", "Google", "LG", "Motorola", "OnePlus"];
 
 const GraphPage = () => {
   return (
     <Wrapper>
-            <div className="dashboard-right_chart-header">
-              <div className="overview-details user-count">
-                <div className="count-text user-count-num"><img style={{"marginBottom":"2.8px"}} src={user} alt="user"/>User Number</div>
-                <div className="count-num">1052</div>
-              </div>
-              <div className="overview-details item-count">
-                <div className="count-text item-count-num"><img style={{"marginBottom":"2.8px"}} src={products} alt="user"/>Product Number</div>
-                <div className="count-num">52235</div>
-              </div>
-              <div className="overview-details order-count">
-                <div className="count-text order-count-num"><img style={{"marginBottom":"2.8px"}} src={orders} alt="user"/>Order Number</div>
-                <div className="count-num">342</div>
-              </div>
-              <div className="overview-details delivery-count">
-                <div className="count-text delivery-count-num"><img style={{"marginBottom":"2.8px"}} src={delevered} alt="user"/>Delivery Number</div>
-                <div className="count-num">244</div>
-              </div>
-            </div>
-            <div className="dashboard-chart-container">
-              <div className="chart_1">
-                <div className='chart_header'>
-                  <div className='chart_heading'>Chart heading</div>
-                  <div className='chart_query'>
-                    <select>
-                      <option>Select year</option>
-                      <option>2001</option>
-                      <option>2002</option>
-                      <option>2003</option>
-                      <option>2004</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="chart-content">
-                  <CChart
-                    type="bar"
-                    data={{
-                      labels: [
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                      ],
-                      datasets: [
-                        {
-                          label: "GitHub Commits",
-                          backgroundColor: "#f87979",
-                          data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
-                        },
-                      ],
-                    }}
-                    labels="months"
-                  />
-                </div>
-              </div>
-              <div className="chart_2">
-                <div className="chart-content-pie">
-                <PieChart
-                  
-                  data={[
-                    { title: "Apple", value: 10, color: "#1FA2FF",label:"one" },
-                    { title: "Samsung", value: 15, color: "#f5af19",label:"two" },
-                    { title: "Google", value: 20, color: "#12D8FA",label:"three" },
-                    { title: "LG", value: 10, color: "#E14D2A",label:"three" },
-                    { title: "Motorola", value: 31, color: "#FD841F",label:"three" },
-                    { title: "OnePlus", value: 36, color: "#3E6D9C",label:"three" },
-                  ]}
-                  animate={true}
-                />
-                <div className='pie-chart-desc'>
-                  <div></div>
-                </div>
-                </div>
-              </div>
-            </div>
-            <div className="dashboard-chart-container">
-              <div className="chart_1">
-              <div className='chart_header'>
-                  <div className='chart_heading'>Chart heading</div>
-                  <div className='chart_query'>
-                    <select>
-                      <option>Select year</option>
-                      <option>2001</option>
-                      <option>2002</option>
-                      <option>2003</option>
-                      <option>2004</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="chart-content">
-                  <CChart
-                    type="bar"
-                    data={{
-                      labels: [
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                      ],
-                      datasets: [
-                        {
-                          label: "GitHub Commits",
-                          backgroundColor: "#1FA2FF",
-                          data: [80, 20, 12, 39, 10, 40, 39, 80, 40],
-                        },
-                      ],
-                    }}
-                    labels="months"
-                  />
-                </div>
-              </div>
-              <div className="chart_2">
-                <div className="chart-content">
-                  <CChart
-                    type="line"
-                    data={{
-                      labels: [
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                      ],
-                      datasets: [
-                        {
-                          label: "My First dataset",
-                          backgroundColor: "rgba(220, 220, 220, 0.2)",
-                          borderColor: "rgba(220, 220, 220, 1)",
-                          pointBackgroundColor: "rgba(220, 220, 220, 1)",
-                          pointBorderColor: "#fff",
-                          data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
-                        },
-                        {
-                          label: "My Second dataset",
-                          backgroundColor: "rgba(151, 187, 205, 0.2)",
-                          borderColor: "rgba(151, 187, 205, 1)",
-                          pointBackgroundColor: "rgba(151, 187, 205, 1)",
-                          pointBorderColor: "#fff",
-                          data: [50, 12, 28, 29, 7, 25, 12, 70, 60],
-                        },
-                      ],
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="dashboard-chart-container">
-              <div className="chart_1">
-                <div className="chart-content">
-                  <PieChart
-                  
-                    data={[
-                      { title: "One", value: 10, color: "#1FA2FF",label:"one" },
-                      { title: "Two", value: 15, color: "#f5af19",label:"two" },
-                      { title: "Three", value: 20, color: "#12D8FA",label:"three" },
-                    ]}
-                    animate={true}
-                  />
-                </div>
-              </div>
-              <div className="chart_2">
-                <div className="chart-content">
-                  <CChart
-                    type="line"
-                    data={{
-                      labels: [
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                      ],
-                      datasets: [
-                        {
-                          label: "My First dataset",
-                          backgroundColor: "rgba(220, 220, 220, 0.2)",
-                          borderColor: "rgba(220, 220, 220, 1)",
-                          pointBackgroundColor: "rgba(220, 220, 220, 1)",
-                          pointBorderColor: "#fff",
-                          data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
-                        },
-                        {
-                          label: "My Second dataset",
-                          backgroundColor: "rgba(151, 187, 205, 0.2)",
-                          borderColor: "rgba(151, 187, 205, 1)",
-                          pointBackgroundColor: "rgba(151, 187, 205, 1)",
-                          pointBorderColor: "#fff",
-                          data: [50, 12, 28, 29, 7, 25, 12, 70, 60],
-                        },
-                      ],
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-          </Wrapper>
-  )
-}
+      <div className="dashboard-right_chart-header">
+        <div className="overview-details user-count">
+          <div className="count-text user-count-num">
+            <img style={{ marginBottom: "2.8px" }} src={user} alt="user" />
+            User Number
+          </div>
+          <div className="count-num">1052</div>
+        </div>
+        <div className="overview-details item-count">
+          <div className="count-text item-count-num">
+            <img style={{ marginBottom: "2.8px" }} src={products} alt="user" />
+            Product Number
+          </div>
+          <div className="count-num">52235</div>
+        </div>
+        <div className="overview-details order-count">
+          <div className="count-text order-count-num">
+            <img style={{ marginBottom: "2.8px" }} src={orders} alt="user" />
+            Order Number
+          </div>
+          <div className="count-num">342</div>
+        </div>
+        <div className="overview-details delivery-count">
+          <div className="count-text delivery-count-num">
+            <img style={{ marginBottom: "2.8px" }} src={delevered} alt="user" />
+            Delivery Number
+          </div>
+          <div className="count-num">244</div>
+        </div>
+      </div>
+      <div className="dashboard-chart-container">
+        <BarChart />
+        <PieDataChart pieChartData={pieChartData} />
+      </div>
+      <div className="dashboard-chart-container">
+        <BarChart />
+        <LineChart />
+      </div>
+      <div className="dashboard-chart-container">
+        <PieDataChart pieChartData={pieChartData} />
+        <LineChart />
+      </div>
+    </Wrapper>
+  );
+};
 
-export default GraphPage
+export default GraphPage;
